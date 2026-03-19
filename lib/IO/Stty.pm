9bd6baf4a5e5ddde7988c204df5a5de9e6250a0f
Todd Rinaldo <toddr@cpan.org>
2026-03-19 08:51:09 -0600

    Bump version to 0.05 for release to CPAN

--- lib/IO/Stty.pm
+++ lib/IO/Stty.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use POSIX;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 # Baud rate constants: standard POSIX rates plus modern rates.
 # Modern rates (B57600, B115200, B230400) are not available on all platforms,
